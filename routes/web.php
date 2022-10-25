547d094bdd04fd745a593cac07f2d20fde57b811
shotej007 <muktadir.shotej@gmail.com>
2022-10-26 05:29:20 +0600

    .

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -41,3 +45,14 @@ Route::fallback(function (){ return "<h1>Sorry the page does not exists</h1>";})
 //        return "All Companies";
 //    }
 //});
+
+Route::resource('/companies', CompanyController::class);
+
+Route::resources([
+    '\tags'=>TagController::class,
+    '\tasks'=>TaskController::class,
+]);
+
+Route::resource('\activities',ActivityController::class)->except([
+    'index', 'show'
+]);
